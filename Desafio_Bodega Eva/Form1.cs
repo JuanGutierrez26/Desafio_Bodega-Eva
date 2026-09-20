470070422bfa45e0a513e78644cb97e658645957
JuanG <juanjosegrtrabajos@gmail.com>
2026-09-20 11:05:01 -0500

    Agregar archivos de proyecto.

---
+++ Desafio_Bodega Eva/Form1.cs
@@ -0,0 +1,10 @@
+namespace Desafio_Bodega_Eva
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Desafio_Bodega Eva/Modelos/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bodega_Eva.Modelos
+{
+    internal class Pedido
+    {
+        // Representa un pedido realizado por un cliente,
+        // principalmente los pedidos recibidos mediante WhatsApp.
+    }
+}
