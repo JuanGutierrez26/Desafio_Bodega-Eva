2b76b3b031dfa48456eab17d74a25abdc986f864
JuanG <juanjosegrtrabajos@gmail.com>
2026-09-20 14:20:47 -0500

    feat: add main form button

--- Desafio_Bodega Eva/FormPrincipal.cs
+++ Desafio_Bodega Eva/FormPrincipal.cs
@@ -1,3 +1,5 @@
+using Desafio_Bodega_Eva.Formularios;
+
 namespace Desafio_Bodega_Eva
 {
     public partial class FormPrincipal : Form
@@ -5,6 +7,23 @@ namespace Desafio_Bodega_Eva
         public FormPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void btnPedidos_Click(object sender, EventArgs e)
+        {
+            FormPedidos form = new FormPedidos();
+            form.ShowDialog();
+        }
+
+        private void btnVentas_Click(object sender, EventArgs e)
+        {
+            FormVentas form = new FormVentas();
+            form.ShowDialog();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
